6cb22346b19233790613bfe94eea2f7bcf3972f6
Aleksandr Kuznietsov <7276766@gmail.com>
2026-06-25 22:43:51 +0300

    telegram bot

---
+++ cmd/tgbot/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"gopkg.in/telebot.v3"
+)
+
+type createResponse struct {
+	Data struct {
+		ShortCode string `json:"short_code"`
+	} `json:"data"`
+}
+
+func main() {
+
+	bot, err := telebot.NewBot(telebot.Settings{
+		Token:  "8941887985:AAHIejP8u_l2ahgqjf_EFahdrHJsq6x8Ziw",
+		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Handle(telebot.OnText, func(c telebot.Context) error {
+		url := c.Text()
+		body := strings.NewReader(`{"original_url": "` + url + `"}`)
+
+		resp, err := http.Post("http://localhost:8080/api/v1/links", "application/json", body)
+		if err != nil {
+			return err
+		}
+
+		var result createResponse
+		json.NewDecoder(resp.Body).Decode(&result)
+		return c.Send("http://localhost:8080/" + result.Data.ShortCode)
+	})
+
+	bot.Start()
+}
--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.12.3
 )
+
+require gopkg.in/telebot.v3 v3.3.8
